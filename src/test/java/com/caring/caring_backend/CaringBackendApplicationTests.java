f2aa6a8cd974743d472e6666ee1b98743249778d
seyeon09 <tpdus0919@dongyang.ac.kr>
2026-05-18 10:44:13 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/caring/caring_backend/CaringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caring.caring_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
