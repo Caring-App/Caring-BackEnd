e21c5e95c875c733bbc479edfb7e33bb100b85a5
seyeon09 <tpdus0919@dongyang.ac.kr>
2026-05-27 14:27:21 +0900

    feat: ConnectionController 구현

---
+++ src/main/java/com/caring/domain/connection/controller/ConnectionController.java
@@ -0,0 +1,30 @@
+package com.caring.domain.connection.controller;
+
+import com.caring.domain.connection.dto.ConnectionRequestDto;
+import com.caring.domain.connection.dto.ConnectionResponseDto;
+import com.caring.domain.connection.service.ConnectionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/connection")
+public class ConnectionController {
+
+    private final ConnectionService connectionService;
+
+    @PostMapping
+    public ResponseEntity<ConnectionResponseDto> connect(
+            @AuthenticationPrincipal Long protectorId,
+            @RequestBody ConnectionRequestDto requestDto){
+
+        // 서비스에게 넘겨서 보호자, 돌봄대상자 연결 처리
+        ConnectionResponseDto response = connectionService.connect(protectorId,requestDto);
+        return ResponseEntity.ok(response);
+    }
+}
