b7c49ef7c24752d21dcf5445c22fba50f02656f1
2nayeon <leenayeon0915@gmail.com>
2026-07-08 14:19:43 +0900

    fix: 복약 스케줄 등록 시 isActive 값이 저장 안되는 문제 수정

--- src/main/java/com/caring/domain/pill/dto/PillScheduleRequestDto.java
+++ src/main/java/com/caring/domain/pill/dto/PillScheduleRequestDto.java
@@ -2,6 +2,7 @@ package com.caring.domain.pill.dto;
 
 import com.caring.domain.pill.entity.AlarmType;
 import com.caring.domain.pill.entity.PillType;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,5 +18,7 @@ public class PillScheduleRequestDto {
     private Integer retryAlarm;
     private AlarmType alarmType;
     private String voiceFileUrl;
+
+    @JsonProperty("isActive")
     private boolean isActive;
 }
