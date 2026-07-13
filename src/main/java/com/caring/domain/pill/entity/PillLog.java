db8214e6b2864b817c4e8bd2fb48fdd071710c01
seyeon09 <tpdus0919@dongyang.ac.kr>
2026-07-13 13:33:42 +0900

    feat: PillLog 엔티티 추가

---
+++ src/main/java/com/caring/domain/pill/entity/PillLog.java
@@ -0,0 +1,38 @@
+package com.caring.domain.pill.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name="pill_log")
+public class PillLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pill_log_id")
+    private Long pillLogId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pill_schedule_id")
+    private PillSchedule pillSchedule;
+
+    @Column(name = "record_date")
+    private LocalDate recordDate;
+
+    @Builder.Default
+    @Column(name = "is_taken")
+    private boolean isTaken = false;
+
+    @Column(name = "confirmed_at")
+    private LocalDateTime confirmedAt;
+
+    @Builder.Default
+    @Column(name = "current_retry_count")
+    private Integer currentRetryCount = 0;
+}
