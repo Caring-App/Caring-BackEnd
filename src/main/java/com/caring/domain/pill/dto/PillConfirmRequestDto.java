aedeae63e37cd46d8761758ee7abc7a950daeb43
seyeon09 <tpdus0919@dongyang.ac.kr>
2026-07-13 13:44:03 +0900

    feat: 복약 확인 요청 DTO 추가

---
+++ src/main/java/com/caring/domain/pill/dto/PillConfirmRequestDto.java
@@ -0,0 +1,10 @@
+package com.caring.domain.pill.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PillConfirmRequestDto {
+    private Long pillLogId;
+}
