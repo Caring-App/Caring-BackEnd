47c3ac5ba5af8af7891c29aa387bd1e1d5c3d48e
seyeon09 <tpdus0919@dongyang.ac.kr>
2026-07-13 10:53:11 +0900

    feat: 소셜 로그인 엔드포인트 추가 (POST /api/auth/{provider})

--- src/main/java/com/caring/domain/member/controller/AuthController.java
+++ src/main/java/com/caring/domain/member/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.caring.domain.member.controller;
 
 import com.caring.domain.member.dto.*;
+import com.caring.domain.member.entity.Provider;
 import com.caring.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -66,4 +67,18 @@ public class AuthController {
         LoginResponseDto responseDto = memberService.login(requestDto);
         return ResponseEntity.ok(responseDto);
     }
+
+    /*
+     * 소셜 로그인
+     * POST http://localhost:8080/api/auth/{provider}
+     * 예: POST /api/auth/kakao
+     */
+    @PostMapping("/{provider}")
+    public ResponseEntity<SocialLoginResponseDto> socialLogin(
+            @PathVariable("provider")Provider provider, // // URL의 {provider} 부분을 자동으로 Provider enum으로 변환해줌
+            @RequestBody SocialLoginRequestDto requestDto){
+        SocialLoginResponseDto responseDto = memberService.socialLogin(provider,requestDto);
+        return ResponseEntity.ok(responseDto);
+    }
+
 }
