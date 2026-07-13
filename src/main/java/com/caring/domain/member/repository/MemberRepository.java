d8759a1e7c074def23e4d68e724e6723e9d61f2d
seyeon09 <tpdus0919@dongyang.ac.kr>
2026-07-13 09:41:58 +0900

    feat: provider+providerId로 회원 조회하는 메서드 추가

--- src/main/java/com/caring/domain/member/repository/MemberRepository.java
+++ src/main/java/com/caring/domain/member/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.caring.domain.member.repository;
 
 import com.caring.domain.member.entity.Member;
+import com.caring.domain.member.entity.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByPhone(String phone);
-    Optional<Member> findByProtectorCode(String protectorCode);}
+    Optional<Member> findByProtectorCode(String protectorCode);
+    Optional<Member> findByProviderAndProviderId(Provider provider, String providerId);}
