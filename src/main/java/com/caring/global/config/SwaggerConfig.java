85a52ad0bb3a853dc75e8494751446397a4e6c17
2nayeon <leenayeon0915@gmail.com>
2026-07-14 11:47:19 +0900

    fix: SwaggerConfig Header에 AccessToken 입력 설정 추가

--- src/main/java/com/caring/global/config/SwaggerConfig.java
+++ src/main/java/com/caring/global/config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package com.caring.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +13,23 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI caringOpenApi() {
+        String jwtSchemeName = "JWT";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Caring API")
                         .description("Caring 플랫폼 API 명세서")
-                        .version("v0.0.1"));
+                        .version("v0.0.1"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 }
