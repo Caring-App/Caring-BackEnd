415f4a4264014f97a555f0f162d605b2647f3af2
2nayeon <leenayeon0915@gmail.com>
2026-07-23 14:06:18 +0900

    feat: 캘린더 일정 TTS 발송 스케줄러 구현

--- src/main/java/com/caring/domain/schedule/repository/TaskScheduleRepository.java
+++ src/main/java/com/caring/domain/schedule/repository/TaskScheduleRepository.java
@@ -4,8 +4,10 @@ import com.caring.domain.schedule.entity.TaskSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public interface TaskScheduleRepository extends JpaRepository<TaskSchedule, Long> {
     List<TaskSchedule> findByWard_MemberIdAndTaskDateOrderByTaskTimeAsc(Long wardId, LocalDate taskDate);
+    List<TaskSchedule> findByTaskDateAndTtsVoiceTime(LocalDate taskDate, LocalTime ttsVoiceTime);
 }
