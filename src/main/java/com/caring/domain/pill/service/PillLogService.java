366c6939ee33244d04c7a149b32324e9e31a9efd
lee5eyeon <leeseny09@gmail.com>
2026-08-27 09:42:31 +0900

    feat: 오늘자 복약 상태 조회 로직 구현 (getTodayPillStatus)

--- src/main/java/com/caring/domain/pill/service/PillLogService.java
+++ src/main/java/com/caring/domain/pill/service/PillLogService.java
@@ -5,13 +5,21 @@ import com.caring.domain.member.entity.Member;
 import com.caring.domain.notification.service.FcmService;
 import com.caring.domain.notification.service.NotificationLogService;
 import com.caring.domain.pill.dto.PillLogResponseDto;
+import com.caring.domain.pill.dto.PillTodayResponseDto;
 import com.caring.domain.pill.entity.PillLog;
 import com.caring.domain.pill.entity.PillSchedule;
 import com.caring.domain.pill.repository.PillLogRepository;
+import com.caring.domain.pill.repository.PillScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +30,7 @@ public class PillLogService {
     private final ConnectionRepository connectionRepository;
     private final FcmService fcmService;
     private final NotificationLogService notificationLogService;
+    private final PillScheduleRepository pillScheduleRepository;
 
     @Transactional
     public PillLogResponseDto confirmPill(Long wardId, Long pillLogId){
@@ -73,5 +82,36 @@ public class PillLogService {
                 }
             }
         }, () -> log.warn("[복약 완료 알림 패스] 대상자 {}와 연결된 보호자가 없습니다.", ward.getName()));
+    }
+
+    // 오늘자 대상자의 복약 상태 조회
+    public List<PillTodayResponseDto> getTodayPillStatus(Long wardId){
+
+        LocalDate today = LocalDate.now();
+        String todayKr = LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREAN);
+
+        List<PillSchedule> pillSchedules = pillScheduleRepository.findByWard_MemberIdAndIsActiveTrue(wardId);
+
+        List<PillTodayResponseDto> result = new ArrayList<>();
+
+        for(PillSchedule pillSchedule : pillSchedules){
+
+            boolean isScheduledToday = pillSchedule.getTakeDays().equals("EVERYDAY")
+                    || pillSchedule.getTakeDays().contains(todayKr);
+
+            if(!isScheduledToday){
+                continue;
+            }
+
+            PillLog pillLog = pillLogRepository.findByPillScheduleAndRecordDate(pillSchedule,today)
+                    .orElseGet(()-> pillLogRepository.save(
+                            PillLog.builder()
+                                    .pillSchedule(pillSchedule)
+                                    .recordDate(LocalDate.now())
+                                    .build()));
+
+            result.add(new PillTodayResponseDto(pillSchedule,pillLog));
+        }
+        return result;
     }
 }
